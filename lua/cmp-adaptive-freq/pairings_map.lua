50a42d663daf3e1f74db73c1031756d60e367b89
TheShadowblast123 <TheShadowblast123@yahoo.com>
2025-08-14 23:01:40 -0700

    Update pairings_map.lua

--- lua/cmp-adaptive-freq/pairings_map.lua
+++ lua/cmp-adaptive-freq/pairings_map.lua
@@ -14,7 +14,7 @@ function Pairing_Map.new()
     local self = setmetatable({}, Pairing_Map)
     
     -- Initialize CMS with 2-bit counters (4 levels: 0-3)
-    self.cms = CMS.new(5, 512, 2,)  -- depth=5, width=512, 2-bit counters
+    self.cms = CMS.new(5, 512, 2)  -- depth=5, width=512, 2-bit counters
     
     -- ID mapping tables
     self.id_map = {}       -- word_id -> cms_key
